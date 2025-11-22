f407eb719bccb572ae1ba90f1a6d21cbf2c39033
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2025-11-22 17:48:25 +0100

    moved the html to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 import "./styles.css";
 
-console.log("ME LOVE YOU :)")
+const homePage = document.querySelector("#content");
+
+const title = document.createElement("h1");
+title.innerText = "Awesome Restaurant";
+const background = document.createElement("div");
+const aboutUs = document.createElement("div");
+
+const aboutUsPraragraf = document.createElement("p");
+aboutUsPraragraf.innerText = "WOW this is so Awsome like i mean REALY AWESOME SO MORE TEXT i need more text more more more okay this is good right? if you wanna continue clik Read";
+const aboutUsButton = document.createElement("button");
+aboutUsButton.innerText = "Read";
+
+aboutUs.appendChild(aboutUsPraragraf)
+aboutUs.appendChild(aboutUsButton)
+
+title.classList.add("title");
+background.classList.add("background");
+aboutUs.classList.add("about_us");
+
+homePage.appendChild(title);
+homePage.appendChild(background);
+homePage.appendChild(aboutUs);
+
